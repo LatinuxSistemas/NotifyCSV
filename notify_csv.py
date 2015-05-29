490fce21280e5696bbeef7267e06f6c2b2e88cbe
Gabriel Davini <gdavini@latinux.com.ar>
2015-05-29 18:02:00 -0300

    [ADD] Add event handler and a basic csv parsing rule

--- notify_csv.py
+++ notify_csv.py
@@ -22,24 +22,122 @@
 #
 ##############################################################################
 
-import sh  # fades.pypi
-import pyinotify  # fades.pypi
 import csv
+import os
+import re
+import sys
+
+import sh  # fades.pypi
+from pyinotify import (IN_CLOSE_WRITE, IN_DELETE, IN_DELETE_SELF,  # fades.pypi
+                       Notifier, ProcessEvent, WatchManager)
+
+
+class EventHandler(ProcessEvent):
+
+    def my_init(self, logger=None, dir_to_watch=None, masks=None, regex='.*', delimiter='\t'):
+        self.logger = logger
+        self.logger.info("Starting event handler")
+        self.regex = regex
+        self.delimiter = delimiter
+        self._create_dir(dir_to_watch)
+        self._fieldnames = ("name", "surname", "birthdate", "city", "country")
+        if masks is None:
+            self.masks = IN_CLOSE_WRITE | IN_DELETE | IN_DELETE_SELF
+        else:
+            self.masks = masks
+        wm = WatchManager()
+        wm.add_watch(self.dir_to_watch, self.masks)
+        notifier = Notifier(wm, self)
+        notifier.loop()
+
+    @property
+    def regex(self):
+        return self._regex
+
+    @regex.setter
+    def regex(self, regex):
+        self._regex = re.compile(regex)
+
+    @property
+    def fieldnames(self):
+        return self._fieldnames
+
+    @fieldnames.setter
+    def fieldnames(self, values):
+        self._fieldnames = values
+
+    def _create_dir(self, dir_to_watch):
+        if not os.path.exists(dir_to_watch):
+            self.logger.info("Creating dir to watch: %s" % dir_to_watch)
+            sh.mkdir(dir_to_watch)
+        self.dir_to_watch = dir_to_watch
+
+    def process_IN_DELETE_SELF(self, event):
+        self.logger.warning("Someone removed %s, we re-create it." % event.pathname)
+        self._create_dir(event.pathname)
+
+    def process_IN_DELETE(self, event):
+        """
+        Write log info when file is deleted
+        """
+        self.logger.info("Deleted file: %s" % event.pathname)
+
+    def process_IN_CLOSE_WRITE(self, event):
+        """
+        When file is written
+        """
+        pathname = event.pathname
+        self.logger.info("close_write On file: %s" % pathname)
+        if re.match(self.regex, pathname):
+            logger.info("CSV file found: %s, let's parse it!" % pathname)
+            with open(pathname, newline='') as csvfile:
+                csvdict = csv.DictReader(
+                    csvfile,
+                    fieldnames=self.fieldnames,
+                    delimiter=self.delimiter,
+                )
+                for line in csvdict:
+                    print(line)
+        return True
 
 if __name__ == '__main__':
-    PROGRAM_NAME = 'NotifyCSV'
     import logging
-    logger = logging.getLogger(PROGRAM_NAME)
-
     from argparse import ArgumentParser
 
-    parser = ArgumentParser(prog=PROGRAM_NAME)
+    prog_name = 'NotifyCSV'
+    default_dir = os.path.join(os.environ['HOME'], "csv-input")
+    logger = logging.getLogger(prog_name)
+    parser = ArgumentParser(prog=prog_name)
 
-    parser.add_argument("-s", "--separator", help="Specify field separator. Default: '\\t'")
-    parser.add_argument("-m", "--match", default='.+.[Cc][Ss][Vv]',
+    parser.add_argument("-d", "--delimiter", help="Specify field delimiter. Default: '\\t'",
+                        default='\t')
+    parser.add_argument("-r", "--regex", default='.+.[Cc][Ss][Vv]',
                         help=("Use this regex to match names in watched dir. Can be any valid "
-                              "Python regex. Default: '.+.[Cc][Ss][Vv]'")
+                              "Python regex. Default: '^.+\.[cC][sS][vV]$'")
                         )
     parser.add_argument("-l", "--logfile", help="Log to this file")
+    parser.add_argument("-w", "--watch-dir", help="Dir to watch. Default: %s" % default_dir,
+                        default=default_dir)
 
     options = parser.parse_args()
+
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
+    logfile = options.logfile
+    if logfile is None:
+        streamH = logging.StreamHandler(sys.stdout)
+    else:
+        streamH = logging.FileHandler(logfile)
+    streamH.setFormatter(formatter)
+    logger.addHandler(streamH)
+
+    logger.info("Started")
+
+    event_handler = EventHandler(
+        logger=logger,
+        dir_to_watch=options.watch_dir,
+        regex=options.regex,
+        delimiter=options.delimiter,
+    )
+    # notifier = EventHandler.notifier
+    # notifier.loop()
